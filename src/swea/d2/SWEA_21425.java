d15cf10a0bc005b50479af21fd869cf19edfb709
seongho Bae <qotjdgh000@gmail.com>
2026-05-29 22:43:07 +0900

    SWEA 21425 풀이

--- src/swea/d2/SWEA_21425.java
+++ src/swea/d2/SWEA_21425.java
@@ -1,9 +1,33 @@
 package swea.d2;
 
+import java.util.Scanner;
+
 public class SWEA_21425 {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		int T, A, B, N, BIG, SMALL, tmp;
+		T = sc.nextInt();
+		int[] result = new int[T];
+		
+		for(int i=0; i<T; i++) {
+			result[i] = 0;
+			A = sc.nextInt();
+			B = sc.nextInt();
+			N = sc.nextInt();
+			
+			BIG = (A>B)?A:B;
+			SMALL = (A>B)?B:A;
+			
+			do {
+				tmp = BIG+SMALL;
+				SMALL = BIG;
+				BIG = tmp;
+				result[i]++;
+			} while(BIG<=N);
+		}
+		
+		for(int j=0; j<T; j++)
+			System.out.println(result[j]);
 	}
-
 }
